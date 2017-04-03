0d647c00f042c570cbe0f7ca319dea5b1a315033
Diarmuid Leahy <DIARMUID.LEAHY.2014@mumail.ie>
2017-04-03 20:02:11 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1 style="text-align:center;text-decoration: underline">Bus Times</h1>
+            <div id="info">
+            </div>
+            <div class="row">
+                <div class="col-md-6" id="display-msg">
+                </div>
+                <div class="col-md-6">
+                    <form class="form-inline pull-right" id="stop_form" method="post" action="index.php">
+                        <input type="text" id="stop" class="form-control mb-2 mr-sm-2 mb-sm-0" value="" name="stop_id" placeholder="Enter a stop number" autofocus>
+                        <button type="submit" class="btn btn-primary" id="go">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script type="text/javascript">
+        $('#stop_form').submit( function (e) {
+            $('#info').slideUp('slow');
+            e.preventDefault();
+            $info = $(this).serializeArray();
+
+            var request = $.ajax({
+              url: "retrieve.php",
+              method: "POST",
+              data: $info,
+              dataType: "html"
+            });
+
+            request.done(function( msg ) {
+                $('#info').html(msg);
+                $('#display-msg').html("<i>Displaying results for Stop number </i><i style='color:green'>"+$('#stop').val()+"</i>");
+                $('#info').slideDown('slow');
+            });
+             
+            request.fail(function( jqXHR, textStatus ) {
+              alert( "Request failed: " + textStatus );
+            });
+
+        });
+    </script>
+</html>
---
+++ retrieve.php
@@ -0,0 +1,28 @@
+<?php
+
+    if(isset($_POST['stop_id'])) {
+        $xml=simplexml_load_file("https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=".$_POST['stop_id']."&format=xml") or die("Error: Cannot create object");
+    }
+
+
+    $message = "<table class='table table-striped table-hover table-responsive'>
+		            <tbody>
+		                <th>Due Time</th>
+		                <th>Route #</th>
+		                <th>Destination</th>
+		                <th>Origin</th>
+		                <th>Operator</th>";
+
+        foreach ($xml->results[0] as $res) {
+            $row = ($res -> duetime == 'Due' ? "<tr class='danger'><td>" : "<tr>
+                        <td>").$res -> duetime.($res -> duetime == 'Due' ? " now</td><td>" : " minutes</td>
+                        <td>").$res -> route."</td>
+                        <td>".$res -> destination."</td>
+                        <td>".$res -> origin."</td>
+                        <td>".$res -> operator."</td>
+                    </tr>";
+            $message .= $row;
+        }
+        $message .= "</tbody></table>";
+        
+    echo $message;
